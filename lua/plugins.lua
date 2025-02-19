eb8da57a7d4fe27c93e41efd1ef8cba2a4a1ebe5
Eric Wong <wsdjeg@outlook.com>
2025-02-19 21:26:06 +0800

    add spc-g-p

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,6 +31,7 @@ require('plug').add({
       vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { silent = true })
       vim.keymap.set('n', '<leader>gv', '<cmd>Git log<cr>', { silent = true })
       vim.keymap.set('n', '<leader>gV', '<cmd>Git log %<cr>', { silent = true })
+      vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { silent = true })
     end,
   },
   {
