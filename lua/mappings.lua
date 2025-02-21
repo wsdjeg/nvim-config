c3fc68d1a77115259612edc3f722b7302356d53a
Eric Wong <wsdjeg@outlook.com>
2025-02-22 02:24:56 +0800

    SPC-q-a -> :qa

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>fs', '<cmd>w<cr>', { silent = true })
 vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>', { silent = true })
+vim.keymap.set('n', '<leader>qa', '<cmd>qa<cr>', { silent = true })
 vim.keymap.set('n', 'so', '<cmd>only<cr>', { silent = true })
 vim.keymap.set('n', 'sp', '<cmd>split<cr>', { silent = true })
 vim.keymap.set('n', 'sv', '<cmd>vsplit<cr>', { silent = true })
