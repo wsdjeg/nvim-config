2e7aa232d317b3b9abc0307c494454d6fb3721d4
Eric Wong <wsdjeg@outlook.com>
2025-02-23 20:13:55 +0800

    fix spc f o

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -127,7 +127,17 @@ require('plug').add({
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
       vim.keymap.set('n', '<F3>', '<cmd>NvimTreeToggle<cr>', { silent = true })
-      vim.keymap.set('n', '<leader>fo', '<cmd>NvimTreeFindFile<cr>', { silent = true })
+      vim.keymap.set('n', '<leader>fo', function()
+        local ok, api = pcall(require, 'nvim-tree.api')
+        if ok then
+          api.tree.change_root(vim.fn.getcwd())
+          api.tree.open({
+            find_file = true,
+          })
+        else
+          vim.cmd('NvimTreeFindFile')
+        end
+      end, { silent = true })
     end,
     config = function()
       require('nvim-tree').setup({
