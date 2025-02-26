b2b31bb1e3b12cb8466f87212f2aea2001230172
Eric Wong <wsdjeg@outlook.com>
2025-02-26 12:27:08 +0800

    enable logger

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,6 +51,7 @@ require('plug').add({
     config = function()
       require('rooter').setup({
         root_pattern = { '.git/' },
+        -- logger = require('logger').derive('rooter')
       })
     end,
   },
