622b72a016a9dccfd2c7779ee392c7b7d8ece2df
Eric Wong <wsdjeg@outlook.com>
2025-03-22 18:00:34 +0800

    set http_proxy & https_proxy

--- init.lua
+++ init.lua
@@ -4,6 +4,13 @@
 -- Author: Eric Wong < wsdjeg@outlook.com >
 -- License: GPLv3
 --=============================================================================
+pcall(function()
+  if vim.fn.sockconnect('tcp', '127.0.0.1:7890') ~= 0 then
+    vim.env.http_proxy = 'http://127.0.0.1:7890' -- set http_proxy=http://127.0.0.1:7890
+    vim.env.https_proxy = 'http://127.0.0.1:7890' -- set https_proxy=http://127.0.0.1:7890
+  end
+end)
+
 require('mappings')
 require('plugins')
 require('autocmds')
