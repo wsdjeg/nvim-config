e3fb07e4d324bc82a889eaf6dc05aaf7ed1d1c9f
Eric Wong <wsdjeg@outlook.com>
2025-03-23 16:23:35 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -90,8 +90,9 @@ require('plug').add({
         local project_root = vim.fn.getcwd()
         local dir = require('ctags.util').unify_path(require('ctags.config').cache_dir)
           .. require('ctags.util').path_to_fname(project_root)
-        local tags =
-          vim.fn.filter(vim.fn.split(vim.o.tags, ','), 'v:val !~# ".cache/SpaceVim/tags"')
+        local tags = vim.tbl_filter(function(t)
+          return not vim.startswith(t, require('ctags.config').cache_dir)
+        end, vim.split(vim.o.tags, ','))
         table.insert(tags, dir .. '/tags')
         vim.o.tags = table.concat(tags, ',')
       end
