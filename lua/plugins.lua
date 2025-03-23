67a8147adeafa82ce914c3ce3768a8ecfee267e3
Eric Wong <wsdjeg@outlook.com>
2025-03-23 16:11:38 +0800

    update dotfile

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -85,6 +85,15 @@ require('plug').add({
     'wsdjeg/ctags.nvim',
     config = function()
       require('ctags').setup()
+
+      local function update_ctags_option()
+        local project_root = vim.fn.getcwd()
+        local dir = require('ctags.util').unify_path(require('ctags.config').cache_dir)
+          .. require('ctags.util').path_to_fname(project_root)
+        table.insert(tags, dir .. '/tags')
+        vim.o.tags = table.concat(tags, ',')
+      end
+      -- require('rooter').reg_callback(update_ctags_option)
     end,
   },
   {
