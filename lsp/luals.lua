53c69d4235ae219fd591772dbed6f8f710513a8e
Eric Wong <wsdjeg@outlook.com>
2025-04-03 21:21:26 +0800

    update lusls config

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -1,3 +1,6 @@
+local lua_rtp = vim.split(package.path, ';')
+table.insert(lua_rtp, 'lua/?.lua')
+table.insert(lua_rtp, 'lua/?/init.lua')
 return {
   cmd = {
     'lua-language-server',
@@ -8,4 +11,37 @@ return {
   },
   root_markers = { '.stylua.toml' },
   filetypes = { 'lua' },
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        path = lua_rtp,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+        disable = { 'need-check-nil' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.tbl_filter(function(var)
+          if string.match(var, 'SpaceVim\\bundle\\neodev.nvim') then
+            return true
+          elseif string.match(var, 'SpaceVim\\bundle') then
+            return false
+          elseif string.match(var, 'wsdjeg\\.cache') then
+            return false
+          end
+          return true
+        end, vim.api.nvim_get_runtime_file('', true)),
+        preloadFileSize = 2000,
+        ignoreDir = { 'bundle' },
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
