0d074880b9d9ff7638c40641ab3a871247211b25
Eric Wong <wsdjeg@outlook.com>
2025-04-05 16:53:16 +0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -133,13 +133,12 @@ cmp.setup({
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
         {
             name = 'dictionary',
             keyword_length = 2,
         },
         { name = 'path' },
-        { name = 'neosnippet' },
+        { name = 'luasnip' },
     }, {
         {
             name = 'buffer',
