efb70e9ccca28ea17b2283054a11bce76ea48033
Eric Wong <wsdjeg@outlook.com>
2025-04-06 21:43:46 +0800

    disable lint

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -628,7 +628,7 @@ require('plug').add({
                 callback = function()
                     -- try_lint without arguments runs the linters defined in `linters_by_ft`
                     -- for the current filetype
-                    require('lint').try_lint()
+                    -- require('lint').try_lint()
                 end,
             })
         end,
