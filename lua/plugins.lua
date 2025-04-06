fb8140f8e6c4fd2c4c440342ee44c2a1d87f8bf7
Eric Wong <wsdjeg@outlook.com>
2025-04-06 19:46:05 +0800

    add nvim-lint

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -621,5 +621,17 @@ require('plug').add({
             })
         end,
     },
+    {
+        'mfussenegger/nvim-lint',
+        config = function()
+            vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+                callback = function()
+                    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+                    -- for the current filetype
+                    require('lint').try_lint()
+                end,
+            })
+        end,
+    },
 })
 require('plug').load()
