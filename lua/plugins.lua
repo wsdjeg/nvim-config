987900c14227514725826ac6f69c97143d2f793c
Eric Wong <wsdjeg@outlook.com>
2025-04-12 22:56:21 +0800

    do not ignore bundle dir

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -79,7 +79,7 @@ require('plug').add({
         config = function()
             require('mru').setup({
                 enable_cache = true,
-                ignore_path_regexs = { '/.git/', '/bundle_dir/' },
+                ignore_path_regexs = { '/.git/' },
                 enable_logger = true,
             })
         end,
