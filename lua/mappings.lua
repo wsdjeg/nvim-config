ae10cb425c7b89833a548b8f15a121be88df1d45
Eric Wong <wsdjeg@outlook.com>
2025-04-13 10:46:02 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,24 +2,31 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>fs', '<cmd>w<cr>', { silent = true })
 vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>', { silent = true })
 vim.keymap.set('n', '<leader>qa', '<cmd>qa<cr>', { silent = true })
+
+-- Windows manager
 vim.keymap.set('n', 'so', '<cmd>only<cr>', { silent = true })
 vim.keymap.set('n', 'sp', '<cmd>split<cr>', { silent = true })
 vim.keymap.set('n', 'sv', '<cmd>vsplit<cr>', { silent = true })
+vim.keymap.set('n', 'q', function()
+    vim.cmd.quit()
+end, { silent = true })
+
+-- Buffer manager
 vim.keymap.set('n', '<leader>bc', function()
-  for i = 1, vim.fn.bufnr('$') do
-    if
-      vim.fn.buflisted(i) == 1
-      and vim.fn.index(vim.fn.tabpagebuflist(), i) == -1
-      and vim.fn.getbufvar(i, '&mod') == 0
-    then
-      vim.cmd(string.format('noautocmd bd %d', i))
+    for i = 1, vim.fn.bufnr('$') do
+        if
+            vim.fn.buflisted(i) == 1
+            and vim.fn.index(vim.fn.tabpagebuflist(), i) == -1
+            and vim.fn.getbufvar(i, '&mod') == 0
+        then
+            vim.cmd(string.format('noautocmd bd %d', i))
+        end
     end
-  end
-  vim.cmd.redrawtabline()
+    vim.cmd.redrawtabline()
 end, { silent = true })
 vim.keymap.set('n', '<leader>bd', function()
-  vim.cmd('bd')
-  vim.cmd.redrawtabline()
+    vim.cmd('bd')
+    vim.cmd.redrawtabline()
 end, { silent = true })
 vim.keymap.set('n', '<leader><tab>', '<cmd>b#<cr>', { silent = true })
 
