65877e0a175f240bb3f6c9eae5b1613dd0da8feb
Eric Wong <wsdjeg@outlook.com>
2025-04-19 16:34:56 +0800

    使用独立文件配置 record-screen

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -669,39 +669,7 @@ require('plug').add({
     {
         'wsdjeg/record-screen.nvim',
         config = function()
-            require('record-screen').setup({
-                cmd = 'ffmpeg',
-                -- 使用 ffmpeg -f dshow -list_devices true -i dummy 获取设备列表
-                -- ffmpeg -f gdigrab -i desktop -i audio="麦克风阵列 (Realtek(R) Audio)" -pix_fmt yuv420p -f mp4
-                argvs = {
-                    '-rtbufsize',
-                    '1500M',
-                    '-f',
-                    'dshow',
-                    '-i',
-                    'audio="麦克风阵列 (Realtek(R) Audio)"',
-                    '-f',
-                    'gdigrab',
-                    '-i',
-                    'desktop',
-                    '-pix_fmt',
-                    'yuv420p',
-                    '-f',
-                    'mp4',
-                },
-            })
-            vim.keymap.set(
-                'n',
-                '<F8>',
-                '<cmd>lua require("record-screen").start()<cr>',
-                { silent = true }
-            )
-            vim.keymap.set(
-                'n',
-                '<F9>',
-                '<cmd>lua require("record-screen").stop()<cr>',
-                { silent = true }
-            )
+            require('plugins.record-screen')
         end,
     },
     {
---
+++ lua/plugins/record-screen.lua
@@ -0,0 +1,34 @@
+
+            require('record-screen').setup({
+                cmd = 'ffmpeg',
+                -- 使用 ffmpeg -f dshow -list_devices true -i dummy 获取设备列表
+                -- ffmpeg -f gdigrab -i desktop -i audio="麦克风阵列 (Realtek(R) Audio)" -pix_fmt yuv420p -f mp4
+                argvs = {
+                    '-rtbufsize',
+                    '1500M',
+                    '-f',
+                    'dshow',
+                    '-i',
+                    'audio="麦克风阵列 (Realtek(R) Audio)"',
+                    '-f',
+                    'gdigrab',
+                    '-i',
+                    'desktop',
+                    '-pix_fmt',
+                    'yuv420p',
+                    '-f',
+                    'mp4',
+                },
+            })
+            vim.keymap.set(
+                'n',
+                '<F8>',
+                '<cmd>lua require("record-screen").start()<cr>',
+                { silent = true }
+            )
+            vim.keymap.set(
+                'n',
+                '<F9>',
+                '<cmd>lua require("record-screen").stop()<cr>',
+                { silent = true }
+            )
