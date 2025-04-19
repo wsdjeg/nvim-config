9c3516f976c69858e05bac41779a8011560bced0
Eric Wong <wsdjeg@outlook.com>
2025-04-19 15:27:08 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -417,7 +417,7 @@ require('plug').add({
         end,
     },
     {
-        'D:/wsdjeg/record-key.nvim',
+        'wsdjeg/record-key.nvim',
         cmds = { 'RecordKeyToggle' },
         config_before = function()
             vim.keymap.set('n', '<leader>rk', '<cmd>RecordKeyToggle<cr>', { silent = true })
