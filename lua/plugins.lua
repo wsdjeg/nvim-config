2ef3247cf2aa05a778472fd55f9cd1dd60aa1a77
Eric Wong <wsdjeg@outlook.com>
2025-04-22 14:06:37 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -568,7 +568,54 @@ require('plug').add({
         'D:/wsdjeg/flygrep.nvim',
         cmds = { 'FlyGrep', 'FlyGrepCword' },
         config = function()
-            require('flygrep').setup()
+            require('flygrep').setup({
+                timeout = 200,
+                color_templete = {
+                    a = {
+                        fg = '#2c323c',
+                        bg = '#98c379',
+                        ctermfg = 16,
+                        ctermbg = 114,
+                        bold = true,
+                    },
+                    b = {
+                        fg = '#abb2bf',
+                        bg = '#3b4048',
+                        ctermfg = 145,
+                        ctermbg = 16,
+                        bold = false,
+                    },
+                },
+
+                command = {
+                    execute = 'rg',
+                    default_opts = {
+                        '--no-heading',
+                        '--color=never',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '-g',
+                        '!.git',
+                    },
+                    expr_opt = '-e',
+                    fixed_string_opt = '-F',
+                    default_fopts = { '-N' },
+                    smart_case = '-S',
+                    ignore_case = '-i',
+                    hidden_opt = '--hidden',
+                },
+
+                matched_higroup = 'IncSearch',
+
+                enable_preview = false,
+                window = {
+                    width = 0.9,
+                    height = 0.9,
+                    col = 0.1,
+                    row = 0.1,
+                },
+            })
             vim.api.nvim_create_user_command('FlyGrepCword', function(opt)
                 require('flygrep').open({ input = vim.fn.expand('<cword>') })
             end, { nargs = '*' })
