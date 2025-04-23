7b0ad410ab3b00129bdce10f316682832f87d5d0
Eric Wong <wsdjeg@outlook.com>
2025-04-23 11:49:49 +0800

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -9,7 +9,7 @@ create_autocmd({ 'BufWritePre' }, {
     end,
 })
 
--- download im-select from https://github.com/daipeihust/im-select
+-- download im-select-mspy from https://github.com/daipeihust/im-select/raw/refs/heads/master/win-mspy/out/x64/im-select-mspy.exe
 local imselect = 'C:\\Users\\wsdjeg\\Downloads\\im-select-mspy.exe'
 
 local function imselect_cn()
@@ -20,14 +20,16 @@ local function imselect_en()
     vim.system({ imselect, '-k=ctrl+space', '英语模式' })
 end
 
-local insert_im
+local buffer_im = {}
+
 create_autocmd({ 'InsertLeave' }, {
     pattern = { '*' },
     group = augroup,
-    callback = function(_)
-        insert_im = vim.fn.trim(vim.fn.system(imselect))
+    callback = function(ev)
         vim.system({ imselect }, { text = true }, function(o)
-            insert_im = vim.trim(vim.iconv(o.stdout, 'cp936', 'utf-8'))
+            -- 这里说明下，再 Windows Terminal 内执行该命令输出的内容默认编码是 `cp936`,
+            -- 需要转码成 utf-8，同时，输出内容尾部有换行符，使用 trim 函数去除。
+            buffer_im[ev.buf] = vim.trim(vim.iconv(o.stdout, 'cp936', 'utf-8'))
         end)
         imselect_en()
     end,
@@ -35,10 +37,12 @@ create_autocmd({ 'InsertLeave' }, {
 create_autocmd({ 'InsertEnter' }, {
     pattern = { '*' },
     group = augroup,
-    callback = function(_)
+    callback = function(ev)
             local c = '英语模式'
-        if insert_im and insert_im ~= '英语模式' then
-            vim.system({ imselect, '-k=ctrl+space', insert_im })
+        if buffer_im[ev.buf] and buffer_im[ev.buf] ~= '英语模式' then
+            -- 此处设置快捷键，可以在输入法按键设置里面查看，我选择的是使用 ctrl-space 切换中英文
+            -- 默认我记得是 shift，同时这个命令默认也是 `-k=shift`
+            vim.system({ imselect, '-k=ctrl+space', buffer_im[ev.buf] })
         end
     end,
 })
