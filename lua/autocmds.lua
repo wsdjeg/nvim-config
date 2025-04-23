2c88c73f508ef24ffeee05cd0f113f2b05e786cd
Eric Wong <wsdjeg@outlook.com>
2025-04-23 09:23:48 +0800

    记录离开输入模式是键盘

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -12,17 +12,47 @@ create_autocmd({ 'BufWritePre' }, {
 -- download im-select from https://github.com/daipeihust/im-select
 local imselect = 'C:\\Users\\wsdjeg\\Downloads\\im-select.exe'
 
-create_autocmd({ 'InsertLeave', 'FocusGained', 'CmdlineLeave', 'VimEnter' }, {
+local function imselect_cn()
+    vim.system({ imselect, '2052' })
+end
+
+local function imselect_en()
+    vim.system({ imselect, '1033' })
+end
+
+local insert_im
+create_autocmd({ 'InsertLeave' }, {
     pattern = { '*' },
     group = augroup,
     callback = function(_)
-        vim.system({imselect, '1033'})
+        insert_im = vim.fn.trim(vim.fn.system(imselect))
+        vim.system({ imselect }, { text = true }, function(o)
+            insert_im = o.stdout
+        end)
+        imselect_en()
+    end,
+})
+create_autocmd({ 'InsertEnter' }, {
+    pattern = { '*' },
+    group = augroup,
+    callback = function(_)
+        if insert_im and insert_im ~= '1033' then
+            vim.system({ imselect, insert_im })
+        end
+    end,
+})
+
+create_autocmd({ 'FocusGained', 'CmdlineLeave', 'VimEnter' }, {
+    pattern = { '*' },
+    group = augroup,
+    callback = function(_)
+        imselect_en()
     end,
 })
 create_autocmd({ 'FocusLost' }, {
     pattern = { '*' },
     group = augroup,
     callback = function(_)
-        vim.system({imselect, '2052'})
+        imselect_cn()
     end,
 })
