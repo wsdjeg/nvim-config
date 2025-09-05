804904fb84663e01dac8c82ab6432f5428213139
Eric Wong <wsdjeg@outlook.com>
2025-09-05 09:22:29 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -313,9 +313,36 @@ require('plug').add({
         },
     },
     {
-        'wsdjeg/scrollbar.vim',
+        'wsdjeg/scrollbar.nvim',
         events = { 'VimEnter' },
-        config = function() end,
+        config = function()
+            require('scrollbar').setup({
+                max_size = 10,
+                min_size = 5,
+                width = 1,
+                right_offset = 1,
+                excluded_filetypes = {
+                    'startify',
+                    'git-commit',
+                    'leaderf',
+                    'NvimTree',
+                    'tagbar',
+                    'defx',
+                    'neo-tree',
+                    'qf',
+                },
+                shape = {
+                    head = '▲',
+                    body = '█',
+                    tail = '▼',
+                },
+                highlight = {
+                    head = 'Normal',
+                    body = 'Normal',
+                    tail = 'Normal',
+                },
+            })
+        end,
     },
     {
         'wsdjeg/iedit.nvim',
