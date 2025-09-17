99ac9b5155c991ecf49bd2e47cd47fd7bf9a1f8b
Eric Wong <wsdjeg@outlook.com>
2025-09-17 22:40:47 +0800

    use bundle repo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,8 +8,7 @@ if vim.fn.isdirectory('D:/bundle_dir/wsdjeg/nvim-plug') == 0 then
         'D:/bundle_dir/wsdjeg/nvim-plug',
     })
 end
--- vim.opt.runtimepath:append('D:/bundle_dir/wsdjeg/nvim-plug')
-vim.opt.runtimepath:append('D:/wsdjeg/nvim-plug')
+vim.opt.runtimepath:append('D:/bundle_dir/wsdjeg/nvim-plug')
 
 require('plug').setup({
 
