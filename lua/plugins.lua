88256a86146648195a8edc2aa7fded87fa02713f
Eric Wong <wsdjeg@outlook.com>
2025-10-08 18:28:45 +0800

    use picker instead of telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -647,6 +647,9 @@ require('plug').add({
                     position = 'top',
                 },
             })
+            vim.keymap.set('n', '<C-p>', '<cmd>Picker files<cr>', { silent = true })
+            vim.keymap.set('n', '<leader>fr', '<cmd>Picker mru<cr>', { silent = true })
+            vim.keymap.set('n', '<leader>bb', '<cmd>Picker buffers<cr>', { silent = true })
         end,
     },
     {
@@ -661,9 +664,6 @@ require('plug').add({
             },
         },
         config_before = function()
-            vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>', { silent = true })
-            vim.keymap.set('n', '<leader>fr', '<cmd>Telescope mru<cr>', { silent = true })
-            vim.keymap.set('n', '<leader>bb', '<cmd>Telescope buffers<cr>', { silent = true })
             vim.keymap.set('n', '<leader>ji', '<cmd>Telescope ctags_outline<cr>', { silent = true })
             vim.keymap.set('n', '<leader>pl', '<cmd>Telescope project<cr>', { silent = true })
         end,
