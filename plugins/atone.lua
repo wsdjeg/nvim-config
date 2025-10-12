52aebd3655cf5feb9ea4985f798b2d93e0af1f14
Eric Wong <wsdjeg@outlook.com>
2025-10-12 13:44:30 +0800

    add undotree plugin atone

---
+++ plugins/atone.lua
@@ -0,0 +1,29 @@
+return {
+    'XXiaoA/atone.nvim',
+    config = function()
+        require('atone').setup({
+            layout = {
+                ---@type "left"|"right"
+                direction = 'left',
+                ---@type "adaptive"|integer|number
+                --- adaptive: exact the width of tree graph
+                --- if number given is a float less than 1, the width is set to `vim.o.columns * that number`
+                width = 0.25,
+            },
+            -- diff for the node under cursor
+            -- shown under the tree graph
+            diff_cur_node = {
+                enabled = true,
+                ---@type number float less than 1
+                --- The diff window's height is set to a specified percentage of the original (namely tree graph) window's height.
+                split_percent = 0.3,
+            },
+            -- automatically update the buffer that the tree is attached to
+            -- only works for buffer whose buftype is <empty>
+            auto_attach = {
+                enabled = true,
+                excluded_ft = { 'oil' },
+            },
+        })
+    end,
+}
