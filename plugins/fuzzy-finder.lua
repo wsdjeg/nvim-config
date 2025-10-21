81b060754ed42385973c9bf297b597625b5229a6
Eric Wong <wsdjeg@outlook.com>
2025-10-21 22:05:19 +0800

    test dir

--- plugins/fuzzy-finder.lua
+++ plugins/fuzzy-finder.lua
@@ -4,4 +4,5 @@ return {
     { 'Shougo/denite.nvim', fetch = true },
     { 'Shougo/unite.vim', fetch = true },
     { 'vim-ctrlspace/vim-ctrlspace', fetch = true },
+    { 'Yggdroot/a_large_project', fetch = true },
 }
