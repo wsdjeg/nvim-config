f5621d10e9ea7bf8b5e7e7027da88a71a5e0049d
Eric Wong <wsdjeg@outlook.com>
2025-10-22 00:10:53 +0800

    disable cmp for snacks_picker_input

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,7 +59,7 @@ local kind_icons = {
 cmp.setup({
     enabled = function()
         -- https://github.com/nvim-tree/nvim-tree.lua/pull/3207
-        if vim.bo.filetype == 'NvimTreeFilter' then
+        if vim.tbl_contains({'NvimTreeFilter', 'snacks_picker_input'}, vim.bo.filetype) then
             return false
         end
         return true
--- plugins/fuzzy-finder.lua
+++ plugins/fuzzy-finder.lua
@@ -1,8 +1,9 @@
 return {
     { 'nvim-telescope/telescope.nvim', fetch = true },
-    { 'folke/snacks.nvim', fetch = true },
+    { 'folke/snacks.nvim', opts = { picker = { enable = true } } },
     { 'Shougo/denite.nvim', fetch = true },
     { 'Shougo/unite.vim', fetch = true },
     { 'vim-ctrlspace/vim-ctrlspace', fetch = true },
     { 'Yggdroot/a_large_project', fetch = true },
+    { 'Yggdroot/LeaderF' },
 }
