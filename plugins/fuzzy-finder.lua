bcfe260723dfb016891aee42ae028dea86306427
Eric Wong <wsdjeg@outlook.com>
2025-10-21 21:28:42 +0800

    add old fuzzy-finder

--- plugins/fuzzy-finder.lua
+++ plugins/fuzzy-finder.lua
@@ -1,4 +1,7 @@
 return {
     { 'nvim-telescope/telescope.nvim', fetch = true },
     { 'folke/snacks.nvim', fetch = true },
+    { 'Shougo/denite.nvim', fetch = true },
+    { 'Shougo/unite.vim', fetch = true },
+    { 'vim-ctrlspace/vim-ctrlspace', fetch = true },
 }
