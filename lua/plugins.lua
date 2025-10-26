1c2ae15679a2bad8715b4733df091b5315aaf9cb
Eric Wong <wsdjeg@outlook.com>
2025-10-26 23:12:11 +0800

    set border of terminal

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -130,24 +130,6 @@ require('plug').add({
         dev = true,
     },
     {
-        'wsdjeg/terminal.nvim',
-        keys = {
-            {
-                'n',
-                "<leader>'",
-                '<cmd>lua require("terminal").open()<cr>',
-                { silent = true, desc = 'open terminal in current path' },
-            },
-            {
-                'n',
-                '<leader>"',
-                '<cmd>lua require("terminal").open(vim.fn.expand("%:p:h"))<cr>',
-                { silent = true, desc = 'open terminal in file path' },
-            },
-        },
-        dev = true,
-    },
-    {
         'hrsh7th/nvim-cmp',
         config = function()
             require('plugins.cmp')
---
+++ plugins/terminal.lua
@@ -0,0 +1,22 @@
+return 
+{
+    'wsdjeg/terminal.nvim',
+    keys = {
+        {
+            'n',
+            "<leader>'",
+            '<cmd>lua require("terminal").open()<cr>',
+            { silent = true, desc = 'open terminal in current path' },
+        },
+        {
+            'n',
+            '<leader>"',
+            '<cmd>lua require("terminal").open(vim.fn.expand("%:p:h"))<cr>',
+            { silent = true, desc = 'open terminal in file path' },
+        },
+    },
+    opts = {
+        border = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    },
+    dev = true,
+}
