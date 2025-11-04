9d32520b378d11ebb84931edf8771e6cfe1c138f
Eric Wong <wsdjeg@outlook.com>
2025-11-04 23:19:09 +0800

    update

--- plugin/shada.lua
+++ plugin/shada.lua
@@ -7,7 +7,15 @@ vim.api.nvim_create_user_command('RemoveShadaTemp', function(opt)
     for _, f in ipairs(vim.fn.globpath(vim.fn.stdpath('data') .. '/shada', '*tmp*', false, true)) do
         status = status + vim.fn.delete(f)
     end
-    if status == 0 then
+    if not opt.bang and status == 0 then
         vim.print('Successfully deleted all temporary shada files')
     end
-end, {})
+end, { bang = true })
+
+vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+    group = vim.api.nvim_create_augroup('fuck_shada_temp', { clear = true }),
+    pattern = { '*' },
+    callback = function()
+        vim.cmd('RemoveShadaTemp!')
+    end,
+})
