0e7f754927ff22362e840b57dfec3d5075f00c71
Eric Wong <wsdjeg@outlook.com>
2025-11-05 22:00:02 +0800

    remove duplicate statusline plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -246,27 +246,6 @@ require('plug').add({
         end,
     },
     {
-        'wsdjeg/statusline.nvim',
-        events = { 'VimEnter' },
-        config = function()
-            require('statusline').register_sections('vcs', function()
-                return '%{ v:lua.require("git.command.branch").current() .. v:lua.require("git.command.pull").status() .. v:lua.require("git.command.push").status() }'
-            end)
-            require('statusline').setup({
-                left_sections = { 'winnr', 'filename', 'vcs' },
-            })
-            for i = 1, 9 do
-                vim.keymap.set(
-                    'n',
-                    '<leader>' .. i,
-                    '<cmd>exe "' .. i .. 'wincmd w"<cr>',
-                    { silent = true }
-                )
-            end
-        end,
-        desc = 'module statusline',
-    },
-    {
         'wsdjeg/dashboard-nvim',
         events = { 'VimEnter' },
         config = function()
