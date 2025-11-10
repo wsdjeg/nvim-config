7847fdc83ed6f23f50551356dc91a245b68bdbee
Eric Wong <wsdjeg@outlook.com>
2025-11-10 10:22:47 +0800

    update keymap desc

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,8 @@ vim.keymap.set(
     '<cmd>Telescope keymaps<cr>',
     { silent = true, desc = 'fuzzy find keymaps' }
 )
-vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>', { silent = true })
-vim.keymap.set('n', '<leader>qa', '<cmd>qa<cr>', { silent = true })
+vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>', { silent = true, desc = ':quit' })
+vim.keymap.set('n', '<leader>qa', '<cmd>qa<cr>', { silent = true, desc = ':qall' })
 vim.keymap.set('n', '<leader>bm', function(opt)
     local buf = vim.api.nvim_create_buf(false, true)
     local msg = vim.fn.split(vim.fn.execute('message'), '\n')
@@ -37,19 +37,19 @@ end, { silent = true, desc = 'open message buffer' })
 
 -- Windows manager
 vim.keymap.set('n', 'so', '<cmd>only<cr>', { silent = true, desc = 'close other windows' })
-vim.keymap.set('n', 'sh', '<cmd>split<cr>', { silent = true })
-vim.keymap.set('n', 'sv', '<cmd>vsplit<cr>', { silent = true })
+vim.keymap.set('n', 'sh', '<cmd>split<cr>', { silent = true, desc = 'split windows' })
+vim.keymap.set('n', 'sv', '<cmd>vsplit<cr>', { silent = true, desc = 'vsplit windows' })
 vim.keymap.set('n', 'q', function()
     pcall(vim.cmd.close)
-end, { silent = true })
+end, { silent = true, desc = ':close' })
 
 -- use alt-h/l to move to previous/next buffer
 vim.keymap.set('n', '<M-h>', function()
     require('tabline').jump('prev')
-end, { silent = true })
+end, { silent = true, desc = 'tabline jump previous' })
 vim.keymap.set('n', '<M-l>', function()
     require('tabline').jump('next')
-end, { silent = true })
+end, { silent = true, desc = 'tabline jump next' })
 
 -- Buffer manager
 vim.keymap.set('n', '<leader>bc', function()
@@ -85,24 +85,24 @@ vim.keymap.set('n', '<leader>bd', function()
         vim.cmd('bd')
         vim.cmd.redrawtabline()
     end
-end, { silent = true })
-vim.keymap.set('n', '<leader><tab>', '<cmd>b#<cr>', { silent = true })
+end, { silent = true, desc = 'delete current buffer' })
+vim.keymap.set('n', '<leader><tab>', '<cmd>b#<cr>', { silent = true, desc = 'switch to b#' })
 
 -- LSP mappings
 
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.references, { silent = true })
-vim.keymap.set('n', '<leader>el', vim.diagnostic.setqflist, { silent = true })
-vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, { silent = true })
-vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, { silent = true })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.references, { silent = true, desc = 'lsp references' })
+vim.keymap.set('n', '<leader>el', vim.diagnostic.setqflist, { silent = true, desc = 'lsp diagnostic setqflist' })
+vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, { silent = true, desc = 'lsp diagnostic goto_next' })
+vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, { silent = true, desc = 'lsp diagnostic goto_prev' })
 
 -- Ctrl+Shift+Up/Down to move up and down
-vim.keymap.set('n', '<C-S-Down>', '<cmd>m .+1<cr>==', { silent = true })
-vim.keymap.set('n', '<C-S-Up>', '<cmd>m .-2<cr>==', { silent = true })
+vim.keymap.set('n', '<C-S-Down>', '<cmd>m .+1<cr>==', { silent = true, desc = 'move current line down' })
+vim.keymap.set('n', '<C-S-Up>', '<cmd>m .-2<cr>==', { silent = true, desc = 'move current line up' })
 vim.keymap.set('i', '<C-S-Down>', '<Esc>:m .+1<cr>==gi', { silent = true })
 vim.keymap.set('i', '<C-S-Up>', '<Esc>:m .-2<cr>==gi', { silent = true })
 vim.keymap.set('v', '<C-S-Down>', ":m '>+1<cr>gv=gv", { silent = true })
 vim.keymap.set('v', '<C-S-Up>', ":m '<-2<cr>gv=gv", { silent = true })
-vim.keymap.set('n', '<C-Down>', '<cmd>wincmd j<cr>', { silent = true })
-vim.keymap.set('n', '<C-Up>', '<cmd>wincmd k<cr>', { silent = true })
-vim.keymap.set('n', '<C-Left>', '<cmd>wincmd h<cr>', { silent = true })
-vim.keymap.set('n', '<C-Right>', '<cmd>wincmd l<cr>', { silent = true })
+vim.keymap.set('n', '<C-Down>', '<cmd>wincmd j<cr>', { silent = true, desc = 'wincmd j' })
+vim.keymap.set('n', '<C-Up>', '<cmd>wincmd k<cr>', { silent = true, desc = 'wincmd k' })
+vim.keymap.set('n', '<C-Left>', '<cmd>wincmd h<cr>', { silent = true, desc = 'wincmd h' })
+vim.keymap.set('n', '<C-Right>', '<cmd>wincmd l<cr>', { silent = true, desc = 'wincmd l' })
