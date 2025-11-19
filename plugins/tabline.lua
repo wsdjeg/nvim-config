5e728bdff567afaefde24e9e42a593e063ee9649
Eric Wong <wsdjeg@outlook.com>
2025-11-19 17:03:02 +0800

    add spc-b-n/p

--- plugins/tabline.lua
+++ plugins/tabline.lua
@@ -9,6 +9,22 @@ for i = 1, 9, 1 do
         { silent = true, desc = string.format('jump to tab %d', i) },
     })
 end
+table.insert(keys, {
+    'n',
+    '<leader>bn',
+    function()
+        require('tabline').jump('next')
+    end,
+    { silent = true, desc = 'jump to next item' },
+})
+table.insert(keys, {
+    'n',
+    '<leader>bp',
+    function()
+        require('tabline').jump('prev')
+    end,
+    { silent = true, desc = 'jump to previous item' },
+})
 return {
     'wsdjeg/tabline.nvim',
     events = { 'VimEnter' },
