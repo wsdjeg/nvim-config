673699664cd5ab48be08f133e9b862ce8e6f5bfb
Eric Wong <wsdjeg@outlook.com>
2025-11-24 09:22:21 +0800

    update

--- plugins/fetch.lua
+++ plugins/fetch.lua
@@ -20,7 +20,7 @@ return {
         fetch = true,
     },
     {
-        'wsdjeg/plugin-utils.nvim',
+        'wsdjeg/utils.nvim',
         fetch = true,
     },
     -- 一些常用仓库，让 nvim-plug 帮我下载并更新，但不加入 vim 插件列表。
