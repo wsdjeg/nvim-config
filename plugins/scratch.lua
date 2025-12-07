b1568678af2ad31a499c42c7ddf01906c5956d67
Eric Wong <wsdjeg@outlook.com>
2025-12-07 19:16:16 +0800

    update scratch.nvim setting

--- plugins/scratch.lua
+++ plugins/scratch.lua
@@ -1,6 +1,16 @@
 return {
     'wsdjeg/scratch.nvim',
     dev = true,
+    keys = {
+        {
+            'n',
+            '<leader>bs',
+            function()
+                require('scratch').create({})
+            end,
+            { silent = true },
+        },
+    },
     opts = {
         directory = 'D:/scratch',
     },
