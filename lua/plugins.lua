575d392d9584f465aa708534b78dd156dcf088b9
Eric Wong <wsdjeg@outlook.com>
2025-12-13 23:29:29 +0800

    split plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,40 +167,6 @@ require('plug').add({
         dev = true,
     },
     {
-        'wsdjeg/scrollbar.nvim',
-        events = { 'UIEnter' },
-        config = function()
-            require('scrollbar').setup({
-                max_size = 10,
-                min_size = 5,
-                width = 1,
-                right_offset = 1,
-                excluded_filetypes = {
-                    'startify',
-                    'git-commit',
-                    'leaderf',
-                    'NvimTree',
-                    'tagbar',
-                    'defx',
-                    'neo-tree',
-                    'qf',
-                },
-                shape = {
-                    head = '▲',
-                    body = '█',
-                    tail = '▼',
-                },
-                highlight = {
-                    head = 'Normal',
-                    body = 'Normal',
-                    tail = 'Normal',
-                },
-            })
-        end,
-        dev = true,
-        desc = 'floating scrollbar',
-    },
-    {
         'wsdjeg/iedit.nvim',
         keys = {
             {
