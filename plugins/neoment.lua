4bd8173b38ac293c812f55c068764a5c61256a2f
Eric Wong <wsdjeg@outlook.com>
2025-12-14 18:17:59 +0800

    updat neoment config

--- plugins/neoment.lua
+++ plugins/neoment.lua
@@ -4,5 +4,21 @@ return {
 
         'nvim-lua/plenary.nvim',
     } },
+    config = function()
+        local nt = require('neoment.notify')
+        local notify = require('notify')
+        nt.info = function(msg)
+            notify.notify(msg)
+        end
+        nt.warning = function(msg)
+            notify.notify(msg, 'WarningMsg')
+        end
+        nt.error = function(msg)
+            notify.notify(msg, 'Error')
+        end
+        local icon = require('neoment.icon')
+        -- U+2022 (•): The standard bullet point; good for a simple, solid dot.
+        icon.dot = '•'
+    end,
     dev = true,
 }
