e8e43ade870b502ca3e822f3588d74b463a1e159
Eric Wong <wsdjeg@outlook.com>
2025-12-14 18:59:36 +0800

    add with_opts function

--- plugins/neoment.lua
+++ plugins/neoment.lua
@@ -16,6 +16,9 @@ return {
         nt.error = function(msg)
             notify.notify(msg, 'Error')
         end
+        nt.with_opts = function(msg, level, opts)
+            notify.notify(msg)
+        end
         local icon = require('neoment.icon')
         -- U+2022 (•): The standard bullet point; good for a simple, solid dot.
         icon.dot = '•'
