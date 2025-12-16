545e0e8f50b9a4432997cd178ce0e736357978b7
Eric Wong <wsdjeg@outlook.com>
2025-12-16 23:51:55 +0800

    update

--- plugins/neoment.lua
+++ plugins/neoment.lua
@@ -11,30 +11,30 @@ return {
 
             -- Icon configuration (all optional)
             icon = {
-                invite = '', -- Icon for room invites
-                buffer = '󰮫', -- Icon for buffer rooms
-                favorite = '', -- Icon for favorite rooms
-                people = '', -- Icon for people/DMs
-                space = '󰴖', -- Icon for spaces
-                room = '󰮧', -- Icon for regular rooms
-                low_priority = '󰘄', -- Icon for low priority rooms
-                reply = '↳', -- Icon for message replies
-                right_arrow = '▶', -- Icon for collapsed sections
-                down_arrow = '▼', -- Icon for expanded sections
-                down_arrow_circle = '', -- Icon for down arrow in circle
-                bell = '󰵛', -- Icon for mentions/highlights
-                dot = '•', -- Icon for unread indicator
-                dot_circle = '', -- Icon for dot in circle
-                border_left = '', -- Icon for left border
-                border_right = '', -- Icon for right border
-                vertical_bar = '│', -- Icon for vertical separator
-                vertical_bar_thick = '┃', -- Icon for thick vertical separator
-                tree_branch = '├', -- Icon for tree branch
-                image = '󰋩', -- Icon for images
-                file = '󰈙', -- Icon for files
-                audio = '', -- Icon for audio files
-                location = '󰍎', -- Icon for location
-                video = '', -- Icon for video files
+                invite = '',
+                buffer = '󰮫',
+                favorite = '',
+                people = '',
+                space = '󰴖',
+                room = '󰮧',
+                low_priority = '󰘄',
+                reply = '↳',
+                right_arrow = '▶',
+                down_arrow = '▼',
+                down_arrow_circle = '',
+                bell = '󰵛',
+                dot = '•',
+                dot_circle = '',
+                border_left = '',
+                border_right = '',
+                vertical_bar = '│',
+                vertical_bar_thick = '┃',
+                tree_branch = '├',
+                image = '󰋩',
+                file = '󰈙',
+                audio = '',
+                location = '󰍎',
+                video = '',
             },
         }
     end,
