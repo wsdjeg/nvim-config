2a960d427c5d8b99c634abe9e64aa13534b2d17a
Eric Wong <wsdjeg@outlook.com>
2025-12-16 19:20:49 +0800

    update neoment config

--- plugins/neoment.lua
+++ plugins/neoment.lua
@@ -5,6 +5,38 @@ return {
         'nvim-lua/plenary.nvim',
     } },
     config = function()
+        vim.g.neoment = {
+            -- Save session data to disk (default: true)
+            save_session = true,
+
+            -- Icon configuration (all optional)
+            icon = {
+                invite = '', -- Icon for room invites
+                buffer = '󰮫', -- Icon for buffer rooms
+                favorite = '', -- Icon for favorite rooms
+                people = '', -- Icon for people/DMs
+                space = '󰴖', -- Icon for spaces
+                room = '󰮧', -- Icon for regular rooms
+                low_priority = '󰘄', -- Icon for low priority rooms
+                reply = '↳', -- Icon for message replies
+                right_arrow = '▶', -- Icon for collapsed sections
+                down_arrow = '▼', -- Icon for expanded sections
+                down_arrow_circle = '', -- Icon for down arrow in circle
+                bell = '󰵛', -- Icon for mentions/highlights
+                dot = '•', -- Icon for unread indicator
+                dot_circle = '', -- Icon for dot in circle
+                border_left = '', -- Icon for left border
+                border_right = '', -- Icon for right border
+                vertical_bar = '│', -- Icon for vertical separator
+                vertical_bar_thick = '┃', -- Icon for thick vertical separator
+                tree_branch = '├', -- Icon for tree branch
+                image = '󰋩', -- Icon for images
+                file = '󰈙', -- Icon for files
+                audio = '', -- Icon for audio files
+                location = '󰍎', -- Icon for location
+                video = '', -- Icon for video files
+            },
+        }
         local nt = require('neoment.notify')
         local notify = require('notify')
         nt.info = function(msg)
@@ -19,9 +51,8 @@ return {
         nt.with_opts = function(msg, level, opts)
             notify.notify(msg)
         end
-        local icon = require('neoment.icon')
-        -- U+2022 (•): The standard bullet point; good for a simple, solid dot.
-        icon.dot = '•'
     end,
+    cmds = { 'Neoment' },
     dev = true,
+    desc = 'matrix client for neovim',
 }
