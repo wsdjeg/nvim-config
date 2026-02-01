1eaa74fca31adf9c0bebb55aeb498e6bf8350abf
Eric Wong <wsdjeg@outlook.com>
2026-02-01 13:10:24 +0800

    use custom filter for tabman

--- plugins/tabman.lua
+++ plugins/tabman.lua
@@ -2,6 +2,21 @@ return {
   'wsdjeg/tabman.nvim',
   dev = true,
   keys = {
-    { 'n', '<leader>tt', '<cmd>Tabman<cr>', { silent = true } },
+    {
+      'n',
+      '<leader>tt',
+      '',
+      {
+        silent = true,
+        callback = function()
+          require('tabman').open({
+            filter = function(win)
+              local buf = vim.api.nvim_win_get_buf(win)
+              return vim.api.nvim_get_option_value('buflisted', { buf = buf })
+            end,
+          })
+        end,
+      },
+    },
   },
 }
