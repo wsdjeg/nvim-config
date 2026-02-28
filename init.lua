b096df476faf3a477bb7c156edc64fe271eb60c7
Eric Wong <wsdjeg@outlook.com>
2026-02-28 14:37:40 +0800

    fuck ai-yuanjing

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@
 --=============================================================================
 pcall(function()
   if vim.fn.sockconnect('tcp', '127.0.0.1:7890') ~= 0 then
-    vim.env.http_proxy = 'http://127.0.0.1:7890' -- set http_proxy=http://127.0.0.1:7890
-    vim.env.https_proxy = 'http://127.0.0.1:7890' -- set https_proxy=http://127.0.0.1:7890
+    -- vim.env.http_proxy = 'http://127.0.0.1:7890' -- set http_proxy=http://127.0.0.1:7890
+    -- vim.env.https_proxy = 'http://127.0.0.1:7890' -- set https_proxy=http://127.0.0.1:7890
   end
 end)
 
