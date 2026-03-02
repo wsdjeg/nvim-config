a3ebcfb69647846e7dd878818ec52f25b6804959
Eric Wong <wsdjeg@outlook.com>
2026-03-02 22:40:16 +0800

    update opts

---
+++ plugins/statuscolumn.lua
@@ -0,0 +1,14 @@
+return {
+  'wsdjeg/statuscolumn.nvim',
+  dev = true,
+  opts = {
+    number = {
+      style = 'hybrid',
+      width = 4,
+    },
+    fold = {
+      open = '▼',
+      closed = '▶',
+    },
+  },
+}
