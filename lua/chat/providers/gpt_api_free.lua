bbd10b767d4340265ad0e5c5d43cb2e1faf56133
Eric Wong <wsdjeg@outlook.com>
2026-03-04 20:42:16 +0800

    fix gpt_api_free provider

--- lua/chat/providers/gpt_api_free.lua
+++ lua/chat/providers/gpt_api_free.lua
@@ -4,6 +4,7 @@ local M = {}
 
 local job = require('job')
 local sessions = require('chat.sessions')
+local config = require('chat.config')
 
 function M.available_models()
   -- 免费版支持gpt-5.2, gpt-5.1, gpt-5, gpt-4o，gpt-4.1一天5次；支持deepseek-r1, deepseek-v3, deepseek-v3-2-exp一天30次，支持gpt-4o-mini，gpt-3.5-turbo，gpt-4.1-mini，gpt-4.1-nano, gpt-5-mini，gpt-5-nano一天200次
@@ -25,30 +26,39 @@ function M.available_models()
   }
 end
 
-function M.request(requestObj)
+function M.request(opt)
   local cmd = {
     'curl',
     '-s',
-    'https://api.chatanywhere.org/v1/chat/completions',
+    'https://free.v36.cm/v1/chat/completions',
     '-H',
     'Content-Type: application/json',
     '-H',
-    'Authorization: Bearer ' .. requestObj.api_key,
+    'Authorization: Bearer ' .. config.config.api_key.gpt_api_free,
     '-X',
     'POST',
     '-d',
-    vim.json.encode({
-      model = requestObj.model,
-      messages = requestObj.messages,
-      stream = true,
-    }),
+    '@-',
   }
 
-  local jobid = job.start(cmd, {
-    on_stdout = requestObj.on_stdout,
-    on_exit = requestObj.on_exit,
+  local body = vim.json.encode({
+    model = sessions.get_session_model(opt.session),
+    messages = opt.messages,
+    stream = true,
+    stream_options = { include_usage = true },
+    tools = require('chat.tools').available_tools(),
   })
-  sessions.set_session_jobid(requestObj.session, jobid)
+
+  local jobid = job.start(cmd, {
+    on_stdout = opt.on_stdout,
+    on_stderr = opt.on_stderr,
+    on_exit = opt.on_exit,
+  })
+  job.send(jobid, body)
+  job.send(jobid, nil)
+  sessions.set_session_jobid(opt.session, jobid)
+
+  return jobid
 end
 
 return M
