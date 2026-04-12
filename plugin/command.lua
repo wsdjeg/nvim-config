c2c9f8b65a9c1ea0af53ba182948a04ae7cc64aa
Eric Wong <wsdjeg@outlook.com>
2026-04-12 22:01:12 +0800

    add LogClear command

--- plugin/command.lua
+++ plugin/command.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command('S', function(opt)
   vim.notify(vim.fn.system(opt.args))
 end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('LogClear', function()
+  require('logger').clearRuntimeLog()
+end, { nargs = '*' })
